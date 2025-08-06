38b5dbe4a407b80c02584e84568f23e4bc9f377a
AgnaldoMCM <agnaldomatheusasm@gmail.com>
2025-08-05 20:40:52 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyDGC0VltsvXlq5V_5jKoaX5X-NLE7mXNuQ",
+  authDomain: "temporada-de-ferias.firebaseapp.com",
+  projectId: "temporada-de-ferias",
+  storageBucket: "temporada-de-ferias.firebasestorage.app",
+  messagingSenderId: "73454276212",
+  appId: "1:73454276212:web:5c765bafb0926b8f564b21",
 };
 
 const app = initializeApp(firebaseConfig);
