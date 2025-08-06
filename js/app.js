f3eee1b7c09db5aca37f09acebef3b4af1ed5707
AgnaldoMCM <agnaldomatheusasm@gmail.com>
2025-08-05 20:51:19 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-// js/app.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+// app.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGC0VltsvXlq5V_5jKoaX5X-NLE7mXNuQ",
@@ -15,37 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const form = document.getElementById("inscricao-form");
-const messageDiv = document.getElementById("message");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const nome = form.nome.value;
-  const idade = form.idade.value;
-  const telefone = form.telefone.value;
-  const email = form.email.value;
-  const local = form.local.value;
-  const alergia = form.Alergia.value;
-
-  try {
-    await addDoc(collection(db, "inscricoes"), {
-      nome,
-      idade,
-      telefone,
-      email,
-      local,
-      alergia,
-      timestamp: new Date()
-    });
-
-    messageDiv.textContent = "✅ Inscrição enviada com sucesso!";
-    messageDiv.style.color = "#a0ffa0";
-    form.reset();
-  } catch (error) {
-    console.error("Erro ao enviar:", error);
-    messageDiv.textContent = "❌ Erro ao enviar inscrição. Tente novamente.";
-    messageDiv.style.color = "#ffaaaa";
-  }
-});
-
+export { db };
