2ab685c632309320f4ec932907d406bf24eecafd
AgnaldoMCM <agnaldomatheusasm@gmail.com>
2025-08-05 20:58:15 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-// script.js
 import { db } from './app.js';
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const form = document.getElementById("formInscricao");
+const form = document.getElementById("inscricao-form");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
+  const idade = document.getElementById("idade").value;
   const telefone = document.getElementById("telefone").value;
+  const email = document.getElementById("email").value;
+  const local = document.getElementById("local").value;
+  const alergia = document.getElementById("Alergia").value;
 
   try {
     await addDoc(collection(db, "inscricoes"), {
       nome,
-      email,
+      idade: Number(idade),
       telefone,
+      email,
+      local,
+      alergia,
       dataEnvio: new Date()
     });
 
-    alert("Inscrição enviada com sucesso!");
+    document.getElementById("message").innerText = "✅ Inscrição enviada com sucesso!";
     form.reset();
   } catch (e) {
-    alert("Erro ao enviar inscrição: " + e.message);
+    document.getElementById("message").innerText = "❌ Erro ao enviar inscrição: " + e.message;
   }
+});
