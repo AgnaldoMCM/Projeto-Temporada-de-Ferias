68006329d603e204b9c4d08bb8ebb937d154bb0c
AgnaldoMCM <agnaldomatheusasm@gmail.com>
2025-09-07 16:18:54 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,1 @@
-// app.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDGC0VltsvXlq5V_5jKoaX5X-NLE7mXNuQ",
-  authDomain: "temporada-de-ferias.firebaseapp.com",
-  projectId: "temporada-de-ferias",
-  storageBucket: "temporada-de-ferias.firebasestorage.app",
-  messagingSenderId: "73454276212",
-  appId: "1:73454276212:web:5c765bafb0926b8f564b21",
-  measurementId: "G-CE7S976N2J"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
